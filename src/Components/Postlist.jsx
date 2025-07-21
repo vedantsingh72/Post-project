eb3a188c921af949f1f7497350fcb1d19d2fc87a
Vedant Singh <vedantsingh0072@gmail.com>
2025-07-22 02:03:29 +0530

    Update Postlist.jsx

--- src/Components/Postlist.jsx
+++ src/Components/Postlist.jsx
@@ -5,21 +5,11 @@ import { PostData } from "./store/Card-store";
 function PostList( ) {
   const { postLine , addDataOnline } = useContext(PostData);
 
-  const handleKaro=()=>{
-    fetch("http://dummyjson.com/posts")
-    .then(res=>res.json())
-    .then(data=>{
-       addDataOnline(data.posts);
-    }
-    )
-  }
-
 
   return (
     <div>
-      <div>
+      <div >
         {postLine.length === 0 && <div>No Posts Yet</div>}
-        <button onClick={()=>handleKaro()}>Get Post</button>
       </div>
 
       <div>
